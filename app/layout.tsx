3289489e0c5044748cad9ceddc0759e76113a478
lykamac <43394847+fuhlicity@users.noreply.github.com>
2025-03-19 16:31:43 +0800

    change og image, shorten page description

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,21 +10,21 @@ const openSans = Open_Sans({
 
 export const metadata: Metadata = {
   title: "Tasteology by AM",
-  description: "Discover the art of cooking with expert tips, mouthwatering recipes, and science-backed techniques. From mastering the perfect sear to balancing flavors like a pro, elevate your kitchen skills and turn every meal into a masterpiece!",
+  description: "Master cooking with expert tips, tasty recipes, and pro techniques. Elevate every meal into a masterpiece!",
   keywords: "cooking, recipes, food blog, gourmet, kitchen tips",
   openGraph: {
     title: 'Tasteology by AM',
-    description: 'Discover the art of cooking with expert tips, mouthwatering recipes, and science-backed techniques. From mastering the perfect sear to balancing flavors like a pro, elevate your kitchen skills and turn every meal into a masterpiece!!',
+    description: "Master cooking with expert tips, tasty recipes, and pro techniques. Elevate every meal into a masterpiece!",
     url: 'https://tasteologybyam.netlify.app/',
     siteName: 'Tasteology by AM',
     images: [
       {
-        url: 'https://tasteologybyam.netlify.app/images/og_image.png',
+        url: 'https://tasteologybyam.netlify.app/images/og_image.jpg',
         width: 800,
         height: 600,
       },
       {
-        url: 'https://tasteologybyam.netlify.app/images/og_image.png',
+        url: 'https://tasteologybyam.netlify.app/images/og_image.jpg',
         width: 1200,
         height: 630,
         alt: 'angelika macapagal website',
